8d481a670c424e1e984a2f40abcf2919ffe51b99
kazuya kun <111524889+kazuyakun07@users.noreply.github.com>
2022-11-15 18:16:31 +0530

    Update myinfo.py

--- NekoRobot/modules/myinfo.py
+++ NekoRobot/modules/myinfo.py
@@ -28,7 +28,7 @@ async def proboyx(event):
     on = await bot.send_file(
         event.chat_id,
         file=file2,
-        caption=f"♡ Hey {betsy}, I'm Neko\n♡ I'm Created By [Prince](tg://user?id=1732814103)\n♡ Click The Button Below To Get Your Info",
+        caption=f"♡ Hey {betsy}, I'm miku\n♡ I'm Created By [kazuya](tg://user?id=1488316798)\n♡ Click The Button Below To Get Your Info",
         buttons=button,
     )
 
