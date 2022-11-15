e3627b89df6fad6e5623376a96d5dcc1e860ee90
kazuya kun <111524889+kazuyakun07@users.noreply.github.com>
2022-11-15 19:10:52 +0530

    Update chatbot.py

--- NekoRobot/modules/chatbot.py
+++ NekoRobot/modules/chatbot.py
@@ -150,7 +150,7 @@ __help__ = """
 *Admins only Commands*:
   • `/Chatbot`*:* Shows chatbot control panel
   
-*Powered By @Programmer_Network*
+*Powered By @cosmos_network*
 """
 
 __mod_name__ = "ChatBot"
