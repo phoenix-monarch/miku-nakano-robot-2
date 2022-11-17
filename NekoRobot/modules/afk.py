f0a6ff5f050a199bdcf1196ff7be193a7a6baf1e
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-11-17 12:48:11 +0000

    Auto Code Formatting

--- NekoRobot/modules/afk.py
+++ NekoRobot/modules/afk.py
@@ -1,48 +1,45 @@
-import html
 import random
 import time
 
-from typing import Optional
-from datetime import datetime
-import humanize
-from telegram import MessageEntity, Update, ParseMode
+from telegram import MessageEntity
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
+from telegram.ext import Filters, MessageHandler, run_async
 
-from NekoRobot import dispatcher
+from NekoRobot import REDIS, dispatcher
 from NekoRobot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from NekoRobot.modules.sql import afk_sql as sql
-from NekoRobot.modules.users import get_user_id
-from NekoRobot import REDIS
-from telegram import Message, User
 from NekoRobot.modules.helper_funcs.readable_time import get_readable_time
-from NekoRobot.modules.helper_funcs.alternate import send_message
+from NekoRobot.modules.users import get_user_id
 
 
 def is_user_afk(userid):
-    rget = REDIS.get(f'is_afk_{userid}')
+    rget = REDIS.get(f"is_afk_{userid}")
     return bool(rget)
 
 
 def start_afk(userid, reason):
-    REDIS.set(f'is_afk_{userid}', reason)
-    
+    REDIS.set(f"is_afk_{userid}", reason)
+
+
 def afk_reason(userid):
-    return strb(REDIS.get(f'is_afk_{userid}'))
+    return strb(REDIS.get(f"is_afk_{userid}"))
+
 
 def end_afk(userid):
-    REDIS.delete(f'is_afk_{userid}')
+    REDIS.delete(f"is_afk_{userid}")
     return True
+
 
 # Helpers
 def strb(redis_string):
     return str(redis_string)
 
+
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
+
 
 @run_async
 def afk(update, context):
@@ -56,13 +53,13 @@ def afk(update, context):
     start_afk_time = time.time()
     reason = args[1] if len(args) >= 2 else "none"
     start_afk(update.effective_user.id, reason)
-    REDIS.set(f'afk_time_{update.effective_user.id}', start_afk_time)
+    REDIS.set(f"afk_time_{update.effective_user.id}", start_afk_time)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text(
-            "{} Is Nᴏᴡ Aᴡᴀʏ!".format(fname))
+        update.effective_message.reply_text("{} Is Nᴏᴡ Aᴡᴀʏ!".format(fname))
     except BadRequest:
         pass
+
 
 @run_async
 def no_longer_afk(update, context):
@@ -71,10 +68,12 @@ def no_longer_afk(update, context):
     if not user:  # ignore channels
         return
 
-    if not is_user_afk(user.id):  #Check if user is afk or not
+    if not is_user_afk(user.id):  # Check if user is afk or not
         return
-    end_afk_time = get_readable_time((time.time() - float(REDIS.get(f'afk_time_{user.id}'))))
-    REDIS.delete(f'afk_time_{user.id}')
+    end_afk_time = get_readable_time(
+        (time.time() - float(REDIS.get(f"afk_time_{user.id}")))
+    )
+    REDIS.delete(f"afk_time_{user.id}")
     res = end_afk(user.id)
     if res:
         if message.new_chat_members:  # dont say msg
@@ -94,7 +93,6 @@ def no_longer_afk(update, context):
             )
         except BaseException:
             pass
-            
 
 
 @run_async
@@ -103,9 +101,11 @@ def reply_afk(update, context):
     userc = update.effective_user
     userc_id = userc.id
     if message.entities and message.parse_entities(
-        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
+        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
+    ):
         entities = message.parse_entities(
-            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
+        )
 
         chk_users = []
         for ent in entities:
@@ -118,8 +118,9 @@ def reply_afk(update, context):
                 chk_users.append(user_id)
 
             elif ent.type == MessageEntity.MENTION:
-                user_id = get_user_id(message.text[ent.offset:ent.offset +
-                                                   ent.length])
+                user_id = get_user_id(
+                    message.text[ent.offset : ent.offset + ent.length]
+                )
                 if not user_id:
                     # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
                     return
@@ -131,8 +132,11 @@ def reply_afk(update, context):
                 try:
                     chat = context.bot.get_chat(user_id)
                 except BadRequest:
-                    print("Error: Could not fetch userid {} for AFK module".
-                          format(user_id))
+                    print(
+                        "Error: Could not fetch userid {} for AFK module".format(
+                            user_id
+                        )
+                    )
                     return
                 fst_name = chat.first_name
 
@@ -150,13 +154,17 @@ def reply_afk(update, context):
 def check_afk(update, context, user_id, fst_name, userc_id):
     if is_user_afk(user_id):
         reason = afk_reason(user_id)
-        since_afk = get_readable_time((time.time() - float(REDIS.get(f'afk_time_{user_id}'))))
+        since_afk = get_readable_time(
+            (time.time() - float(REDIS.get(f"afk_time_{user_id}")))
+        )
         if int(userc_id) == int(user_id):
             return
         if reason == "none":
             res = "{} is Dead!\nLast Liveliness: {} Ago.".format(fst_name, since_afk)
         else:
-            res = "{} is afk!\nReason: {}\nLast seen: {} Ago.".format(fst_name, reason, since_afk)
+            res = "{} is afk!\nReason: {}\nLast seen: {} Ago.".format(
+                fst_name, reason, since_afk
+            )
 
         update.effective_message.reply_text(res)
 
@@ -165,16 +173,20 @@ def __user_info__(user_id):
     is_afk = is_user_afk(user_id)
     text = ""
     if is_afk:
-        since_afk = get_readable_time((time.time() - float(REDIS.get(f'afk_time_{user_id}'))))
+        since_afk = get_readable_time(
+            (time.time() - float(REDIS.get(f"afk_time_{user_id}")))
+        )
         text = "Tʜɪs ᴜsᴇʀ ɪs ᴄᴜʀʀᴇɴᴛʟʏ ᴀғᴋ (ᴀᴡᴀʏ ғʀᴏᴍ ᴋᴇʏʙᴏᴀʀᴅ)."
         text += f"\nLᴀsᴛ Sᴇᴇɴ: {since_afk} Aɢᴏ."
-       
+
     else:
         text = "Tʜɪs ᴜsᴇʀ ᴄᴜʀʀᴇɴᴛʟʏ ɪsɴ'ᴛ ᴀғᴋ (ɴᴏᴛ ᴀᴡᴀʏ ғʀᴏᴍ ᴋᴇʏʙᴏᴀʀᴅ)."
     return text
 
+
 def __stats__():
     return f"• {len(REDIS.keys())} Total Keys in Redis Database."
+
 
 def __gdpr__(user_id):
     end_afk(user_id)
@@ -207,4 +219,4 @@ __handlers__ = [
     (NO_AFK_HANDLER, AFK_GROUP),
     (AFK_REPLY_HANDLER, AFK_REPLY_GROUP),
 ]
-#------ishikki-akabane
+# ------ishikki-akabane
